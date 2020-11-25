1e5d9f4f89175a4291c021b353224e99e4bdf60f
nagulvali <nagulvali555@gmail.com>
2020-11-25 02:18:51 -0800

    updared parameters and formats

--- install_moodle.sh
+++ install_moodle.sh
@@ -3,42 +3,46 @@
 #########################################
 # Bash script to install and setup moodle
 
-## Moodle Variables
-# Moodle database name
-moodle_db="moodle"
-# Moodle database user
-moodle_db_user="moodleuser"
-# Moodle database pass
-moodle_db_pass="MoodlePass@123"
-# Website Name
-moodle_web_name="Example"
-# Web short Name
-moodle_web_short_name="example"
-# Web summary
-moodle_web_summary="example website"
-# Admin Name
-moodle_admin_name="admin"
-# Moodle admin pass
-moodle_admin_pass="Admin@123"
-# Moodle Admin Email
-moodle_admin_email="admin@email.com"
+## MySql Parameters
+MYSQL_ROOT_PASSWORD="Password@123"
 
-## Lets Encrypt Vars
- email="nagulvali555@gmail.com"
- domain="testing.vali.life"
+## PhpMyAdmin Parameters
+PHP_MY_ADMIN_PASSWORD="Admin@123"
+
+## Moodle Parameters
+MOODLE_DATABASE_NAME="moodle"
+MOODLE_DATABASE_USER="moodleuser"
+MOODLE_DATABASE_PASSWORD="MoodlePass@123"
+MOODLE_WEBSITE_NAME="Example"
+MOODLE_WEBSITE_SHORT_NAME="example"
+MOODLE_WEBSITE_SUMMARY="example website"
+MOODLE_WEBSITE_ADMIN_USERNAME="admin"
+MOODLE_WEBSITE_ADMIN_PASSWORD="Admin@123"
+MOODLE_WEBSITE_ADMIN_EMAIL="admin@email.com"
+
+## LetsEncrypt Parameters
+DOMAIN="testing.vali.life"
+REGISTERED_DOMAIN_EMAIL="nagulvali555@gmail.com"
+ 
+
+# Parameter check
+for i in MYSQL_ROOT_PASSWORD PHP_MY_ADMIN_PASSWORD MOODLE_DATABASE_NAME \
+MOODLE_DATABASE_USER MOODLE_DATABSE_PASSWORD MOODLE_WEBSITE_NAME MOODLE_WEBSITE_SHORT_NAME \
+MOODLER_WEBSITE_SUMMARY MOODLE_WEBSITE_ADMIN_USERNAME MOODLE_WEBSITE_ADMIN_PASSWORD MOODLE_WEBSITE_ADMIN_EMAIL
+do
+    if [ -z "${!i}" ]
+    then
+        read -p "$i: " $i
+    fi
+done
+
 
 # Check if running as root
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
+  echo "run sudo $0" 1>&2
   exit 1
 fi
-
-# Ask value for mysql root password and php, letsencrypt
-read -sp 'db_root_password [secretpasswd]: ' db_root_password
-echo
-read -sp 'php_myadmin_password [secretpasswd]: ' php_my_admin_pass
-echo
-echo
 
 # Update and Upgrade packages
 apt-get update -y  \
@@ -47,7 +51,6 @@ apt-get update -y  \
 
 # restart services
 restartService () {
-#FIXME: please rename to a non reserved function name e.g. restartService()
     service=$1
     sudo systemctl restart $service
 }
@@ -65,8 +68,8 @@ installApachePhp () {
 # mysql installation and password setup
 installMysql () {
     export DEBIAN_FRONTEND="noninteractive"
-    echo "mysql-server mysql-server/root_password password $db_root_password" | debconf-set-selections
-    echo "mysql-server mysql-server/root_password_again password $db_root_password" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
     apt-get install mysql-server -y
 }
 
@@ -79,7 +82,6 @@ configureMysql () {
 
 # moodle download and installation
 gitCloneMoodleSources () {
-#FIXME: rename gitCloneMoodleSources()
     cd /opt \
     && sudo git clone git://git.moodle.org/moodle.git \
     && cd moodle \
@@ -90,7 +92,6 @@ gitCloneMoodleSources () {
 }
 
 configureApacheWebContent() {
-# FIXME, extract this into configureApacheWebContent()
     sudo cp -R /opt/moodle /var/www/html/ \
     && sudo mkdir /var/moodledata \
     && sudo chown -R www-data /var/moodledata \
@@ -101,45 +102,40 @@ configureApacheWebContent() {
 
 # Creating moodle Database
 createDbMoodle () {
-#FIXME: please rename to createDbMoodle()
-    pass=$1
-    mysql -u root -p$pass -e\
-    "CREATE DATABASE $moodle_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; \
-    create user $moodle_db_user@'localhost' IDENTIFIED BY \"$moodle_db_pass\"; \
-    GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON $moodle_db.* TO $moodle_db_user@'localhost';"
+    mysql -u root -p$MYSQL_ROOT_PASSWORD -e\
+    "CREATE DATABASE $MOODLE_DATABASE_NAME DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; \
+    create user $MOODLE_DATABASE_USER@'localhost' IDENTIFIED BY \"$MOODLE_DATABASE_PASSWORD\"; \
+    GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON $MOODLE_DATABASE_NAME.* TO $MOODLE_DATABASE_USER@'localhost';"
 }
 
 # install moodle from cli
 installMoodle () {
-    sudo -u www-data php /var/www/html/moodle/admin/cli/install.php --wwwroot="https://$domain" \
-    --dataroot='/var/moodledata' --dbname="$moodle_db" --dbuser="$moodle_db_user" --dbpass="$moodle_db_pass" \
-    --fullname="$moodle_web_name" --shortname="$moodle_web_short_name" --summary="$moodle_web_summary" \
-    --adminuser="$moodle_admin_name" --adminpass="$moodle_admin_pass" --adminemail="$moodle_admin_email" \
+    sudo -u www-data php /var/www/html/moodle/admin/cli/install.php --wwwroot="https://$DOMAIN" \
+    --dataroot='/var/moodledata' --dbname="$MOODLE_DATABASE_NAME" --dbuser="$MOODLE_DATABASE_USER" --dbpass="$MOODLE_DATABASE_PASSWORD" \
+    --fullname="$MOODLE_WEBSITE_NAME" --shortname="$MOODLE_WEBSITE_SHORT_NAME" --summary="$MOODLE_WEBSITE_SUMMARY" \
+    --adminuser="$MOODLE_WEBSITE_ADMIN_USERNAME" --adminpass="$MOODLE_WEBSITE_ADMIN_PASSWORD" --adminemail="$MOODLE_WEBSITE_ADMIN_EMAIL" \
     --non-interactive --agree-license
 }
 
 # install phpmyadmin
 installPhpmyadmin () {
-#FIXME: rename to installPhpmyadmin()
-#    sudo apt-get install -y  phpmyadmin php-mbstring php-zip php-gd php-json php-curl \
-#    && sudo phpenmod mbstring
-
     export DEBIAN_FRONTEND=noninteractive
     echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
     echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
     echo "phpmyadmin phpmyadmin/mysql/admin-user string root" | debconf-set-selections
-    echo "phpmyadmin phpmyadmin/mysql/admin-pass password $db_root_password" | debconf-set-selections
-    echo "phpmyadmin phpmyadmin/mysql/app-pass password $php_my_admin_pass" |debconf-set-selections
-    echo "phpmyadmin phpmyadmin/app-password-confirm password $php_my_admin_pass" | debconf-set-selections
+    echo "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
+    echo "phpmyadmin phpmyadmin/mysql/app-pass password $PHP_MY_ADMIN_PASSWORD" |debconf-set-selections
+    echo "phpmyadmin phpmyadmin/app-password-confirm password $PHP_MY_ADMIN_PASSWORD" | debconf-set-selections
 
     apt-get install -y phpmyadmin php-mbstring php-zip php-gd php-json php-curl \
     && sudo phpenmod mbstring
+#    sudo apt-get install -y  phpmyadmin php-mbstring php-zip php-gd php-json php-curl \
+#    && sudo phpenmod mbstring
 }
 
 
 # by default only security updates are applied
 setup_autoupdates(){
-
   apt-get install -y unattended-upgrades
 
   sed -i.save 's/^[\/]*Unattended-Upgrade::Remove-Unused-Kernel-Packages .*/Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";/' /etc/apt/apt.conf.d/50unattended-upgrades
@@ -157,8 +153,8 @@ apacheVirtualhost () {
     cd /etc/apache2/sites-available
     echo "
 <VirtualHost *:80>
-  ServerName $domain
-#  ServerAlias www.$domain
+  ServerName $DOMAIN
+  #ServerAlias www.$DOMAIN
   DocumentRoot /var/www/html/moodle
 
   <Directory /var/www/html/moodle>
@@ -170,8 +166,8 @@ apacheVirtualhost () {
   ErrorLog /var/log/apache2/error.log
   CustomLog /var/log/apache2/access.log combined
   #RewriteEngine on
-  #RewriteCond %{SERVER_NAME} =www.$domain [OR]
-  #RewriteCond %{SERVER_NAME} =$domain
+  #RewriteCond %{SERVER_NAME} =www.$DOMAIN [OR]
+  #RewriteCond %{SERVER_NAME} =$DOMAIN
   #RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]
 </VirtualHost>" > moodle.conf
 
@@ -179,18 +175,17 @@ apacheVirtualhost () {
     a2dissite 000-default.conf
     a2enmod rewrite
     restartService apache2
-
 }
 
 # Install certbot and install certificates with redirect from http to https
 letsencrypt () {
     sudo apt-get install -y certbot python3-certbot-apache \
-    && sudo certbot --apache -d $domain -m $email -n --agree-tos --redirect
+    && sudo certbot --apache -d $DOMAIN -m $REGISTERED_DOMAIN_EMAIL -n --agree-tos --redirect
 }
 
 # Enable ssl for phpmyadmin
 phpmyadminSsl () {
-    echo "Include /etc/phpmyadmin/apache.conf" | tee -a /etc/apache2/apache2.conf
+#    echo "Include /etc/phpmyadmin/apache.conf" | tee -a /etc/apache2/apache2.conf
     sudo a2enmod ssl
     echo "\$cfg['ForceSSL'] = true;" | tee -a /etc/phpmyadmin/config.inc.php
     restartService apache2
@@ -198,21 +193,27 @@ phpmyadminSsl () {
 
 # Configure php access from specific ips
 phpmyadminConfig () {
-    file="/etc/apache2/conf-enabled/phpmyadmin.conf"
-    sudo sed -i.bakup_`date +%F`-`date +%T` '/\<Directory \/usr\/share\/phpmyadmin\>/a    \
+    file="/etc/apache2/conf-available/phpmyadmin.conf"
+    sudo cp $file $file.bkp
+    sudo sed '/\<Directory \/usr\/share\/phpmyadmin\>/a    \
     Order Deny,Allow\n    Deny from All\n    Allow from 10.1.3.0/24\n    \
     Allow from 192.168.16.0/24\n    Allow from 10.1.4.0/24' $file
+    
+    cd `dirname $file`
+    a2enconf `basename $file`
+    restartService apache2
+
 }
 
 # Display information
 info () {
     echo "################################################" 
-    echo "# "
-    echo "# Moodle URL: https://$domain"
-    echo "# "
-    echo "# phpmyadmin URL: https://$domain/phpmyadmin"
-    echo "# "
-    echo "# "
+    echo "#  "
+    echo "# Moodle URL: https://$DOMAIN"
+    echo "#  "
+    echo "# phpmyadmin URL: https://$DOMAIN/phpmyadmin"
+    echo "#  "
+    echo "#  "
     echo "################################################"
 }
 
@@ -223,7 +224,7 @@ installMysql
 gitCloneMoodleSources
 configureApacheWebContent
 configureMysql
-createDbMoodle $db_root_password
+createDbMoodle
 installMoodle
 installPhpmyadmin
 setup_autoupdates
@@ -234,4 +235,3 @@ phpmyadminConfig
 restartService mysql
 restartService apache2
 info
-
